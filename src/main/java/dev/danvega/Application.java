0aaeeb7852500ac8fcf7e427992b0b1fb9652afa
Dan Vega <danvega@gmail.com>
2024-08-30 13:56:52 -0400

    Adding a bean of type rest client so you can ask for that in every class instead of creating a new one.

--- src/main/java/dev/danvega/Application.java
+++ src/main/java/dev/danvega/Application.java
@@ -2,12 +2,25 @@ package dev.danvega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/*
+	 * Create a single RestClient bean that can be used throughout the application
+	 */
+	@Bean
+	RestClient restClient(RestClient.Builder builder, ClientLoggerRequestInterceptor clientLoggerRequestInterceptor) {
+		return builder
+				.baseUrl("https://jsonplaceholder.typicode.com/")
+				.requestInterceptor(clientLoggerRequestInterceptor)
+				.build();
 	}
 
 }
