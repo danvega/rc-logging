ae875d2fdb0c06af3f191e6033fa36c8993b6ec7
Dan Vega <danvega@gmail.com>
2024-10-09 10:25:36 -0400

    wrapping the original ClientHttpResponse

--- src/main/java/dev/danvega/ClientLoggerRequestInterceptor.java
+++ src/main/java/dev/danvega/ClientLoggerRequestInterceptor.java
@@ -4,25 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @Component
 public class ClientLoggerRequestInterceptor implements ClientHttpRequestInterceptor {
-    
+
     private static final Logger log = LoggerFactory.getLogger(ClientLoggerRequestInterceptor.class);
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
         logRequest(request, body);
-        var response = execution.execute(request, body);
-        logResponse(request,response);
-        return response;
+        ClientHttpResponse response = execution.execute(request, body);
+        return logResponse(request, response);
     }
 
     private void logRequest(HttpRequest request, byte[] body) {
@@ -33,17 +35,59 @@ public class ClientLoggerRequestInterceptor implements ClientHttpRequestIntercep
         }
     }
 
-    private void logResponse(HttpRequest request, ClientHttpResponse response) throws IOException {
+    private ClientHttpResponse logResponse(HttpRequest request, ClientHttpResponse response) throws IOException {
         log.info("Response status: {}", response.getStatusCode());
         logHeaders(response.getHeaders());
-        var responseBody = response.getBody().readAllBytes();
-        if( responseBody.length > 0 ) {
+
+        byte[] responseBody = response.getBody().readAllBytes();
+        if (responseBody.length > 0) {
             log.info("Response body: {}", new String(responseBody, StandardCharsets.UTF_8));
         }
+
+        return new BufferingClientHttpResponseWrapper(response, responseBody);
     }
 
     private void logHeaders(HttpHeaders headers) {
-        headers.forEach((name,values) -> values.forEach(value -> log.info("{}={}",name, value)));
+        headers.forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
     }
 
+    private static class BufferingClientHttpResponseWrapper implements ClientHttpResponse {
+        private final ClientHttpResponse response;
+        private final byte[] body;
+
+        public BufferingClientHttpResponseWrapper(ClientHttpResponse response, byte[] body) {
+            this.response = response;
+            this.body = body;
+        }
+
+        @Override
+        public InputStream getBody() throws IOException {
+            return new ByteArrayInputStream(body);
+        }
+
+        @Override
+        public HttpHeaders getHeaders() {
+            return response.getHeaders();
+        }
+
+        @Override
+        public HttpStatusCode getStatusCode() throws IOException {
+            return response.getStatusCode();
+        }
+
+        @Override
+        public int getRawStatusCode() throws IOException {
+            return response.getRawStatusCode();
+        }
+
+        @Override
+        public String getStatusText() throws IOException {
+            return response.getStatusText();
+        }
+
+        @Override
+        public void close() {
+            response.close();
+        }
+    }
 }
